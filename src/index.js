11dbe3f481bfe64d4a042628b2fb4ef28a9f93e8
landvaettir <javier.villalobosv@sansano.usm.cl>
2020-12-08 23:12:50 -0300

    08/12

--- src/index.js
+++ src/index.js
@@ -5,23 +5,67 @@ import Topnav from './Navbar';
 import Newscontainer from './Newscontainer';
 import Foot from './Footer';
 import reportWebVitals from './reportWebVitals';
+
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route, 
+  Link
+} from 'react-router-dom';
+
+import './Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav, Container} from "react-bootstrap";
  
 ReactDOM.render(
   <React.StrictMode>
-    <Topnav></Topnav>
-    <Newscontainer 
-    imagen="#"
-    titulo="Finalmente...."
-    cuerpo="aasdawas......asd"></Newscontainer>
-    <Newscontainer 
-    imagen="#"
-    titulo="Se descubre que..."
-    cuerpo="aasdawas......asd"></Newscontainer>
-    <Newscontainer 
-    imagen="#"
-    titulo="La real historia de..."
-    cuerpo="aasdawas......asd"></Newscontainer>
-    <Foot></Foot>
+    <Router>
+      <Navbar bg="light" expand="lg" className="navBar">
+        <img src={"ceeinfLogo.png"} className="Logo" alt="ceeinf-logo"/>
+        <Navbar.Brand href="index.html">CEEINF</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link as={Link} to="/noticias">Noticias</Nav.Link>
+            <Nav.Link as={Link} to="#" href="#">Calendario</Nav.Link>
+            <Nav.Link as={Link} to="#" href="#">Asambleas</Nav.Link>
+            <Nav.Link href="https://mallas.labcomp.cl/">Malla</Nav.Link>
+            <Nav.Link as={Link} to="#" href="#">Toma de ramos</Nav.Link>
+            <Nav.Link as={Link} to="#" href="#">Documentos</Nav.Link>
+            <Nav.Link as={Link} to="#" href="#">Foro</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+      <Switch>
+        
+        <Route path="/noticias">
+          <Container fluid>
+          <Newscontainer 
+            imagen="#"
+            titulo="Finalmente...."
+            cuerpo="aasdawas......asd">
+          </Newscontainer>
+          <Newscontainer 
+            imagen="#"
+            titulo="Se descubre que..."
+            cuerpo="aasdawas......asd">
+          </Newscontainer>
+          <Newscontainer 
+            imagen="#"
+            titulo="La real historia de..."
+            cuerpo="aasdawas......asd">
+          </Newscontainer>
+          </Container>
+        </Route>
+
+        <Route path="/calendario">
+        </Route>
+
+      </Switch>
+
+      <Foot></Foot>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
