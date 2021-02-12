a6666b1abbf9cda986da3632223d37447431e6ad
Sebastian Godinez <sebastian.godinez@hotmail.com>
2021-02-11 22:03:08 -0300

    dev: add docker files and folder structure

--- src/Calendario.js
+++
@@ -1,9 +0,0 @@
-import './Calendario.css';
-
-function Calendar(){
-    return(
-        <iframe src="https://calendar.google.com/calendar/embed?src=c_jpb3kvrgf8k4sel84vsuq48f04%40group.calendar.google.com&ctz=America%2FSantiago"></iframe>
-    );
-}
-
-export default Calendar;
